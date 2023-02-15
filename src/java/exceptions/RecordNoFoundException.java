ff0bfc40b5488ce4ab87d6f27e9d4ad95fdc286b
Xiangjie Qu <xqu@gus.pittstate.edu>
2023-02-15 09:58:23 -0600

    init

---
+++ src/java/exceptions/RecordNoFoundException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author 0837099
+ */
+public class RecordNoFoundException extends Exception {
+    public RecordNoFoundException (){
+      super("Record NoFound Exception");  //set message and get message 
+      //create the new contructor for the exception
+      
+    }
+}
