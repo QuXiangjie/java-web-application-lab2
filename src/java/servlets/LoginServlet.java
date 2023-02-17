67094541a813ae953aebbcb6fd2851d3827717e7
Xiangjie Qu <xqu@gus.pittstate.edu>
2023-02-17 09:52:30 -0600

    new change

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -1,86 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlets;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-/**
- *
- * @author 0837099
- */
+import domain.Customer;
+import database.CustomerDA;
+
 public class LoginServlet extends HttpServlet {
+        
+       protected void doPost(HttpServletRequest request, 
+                          HttpServletResponse response) 
+                          throws ServletException, IOException {
+                         //System.out.println("this is the request="+request);
+       //System.out.println("this is the response="+response);
+        String url = "/index.html";
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        // get current action
+        String option = request.getParameter("option");
+        if (option == null) {
+            option = "join";  // default action
         }
-    }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        // perform action and set URL to appropriate page
+        if (option.equals("join")) {
+            url = "/index.html";    // the "join" page
+        }
+        else if (option.equals("add")) {                
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+            // store data in User object and save User object in database
+            Customer  = new User(firstName, lastName, email);
+            user.add();
+            
+            // set User object in request object and set URL
+            request.setAttribute("user", user);
+            url = "/thanks.jsp";   // the "thanks" page
+        }
+        
+        // forward request and response objects to specified URL
+        //System.out.println("this is the getServletContext()"+getServletContext());
+        
+        getServletContext()
+            .getRequestDispatcher(url)
+            .forward(request, response);
+        
+        
+    }    
+    
+    protected void doGet(HttpServletRequest request, 
+                          HttpServletResponse response) 
+                          throws ServletException, IOException {
+        doPost(request, response);
+    }    
 }
